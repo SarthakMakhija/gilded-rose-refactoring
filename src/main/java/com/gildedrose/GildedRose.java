484d6f830dc47ac819110e21a7a9c4a023218e03
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-03-21 16:58:15 +0530

    Sarthak | Introduces nonLegendaryItemMatch

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,16 +1,20 @@
 package com.gildedrose;
 
+import java.util.function.Predicate;
+
 class GildedRose {
     Item[] items;
+    Predicate<Item> nonLegendaryItemMatch;
 
     public GildedRose(Item[] items) {
         this.items = items;
+        this.nonLegendaryItemMatch = ((Predicate<Item>) (Item item) -> item.matchesName("Sulfuras, Hand of Ragnaros")).negate();
     }
 
     public void updateQuality() {
         for (Item item : items) {
             if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.dropQualityByOneIf((Item item1) -> !item1.matchesName("Sulfuras, Hand of Ragnaros"));
+                item.dropQualityByOneIf(this.nonLegendaryItemMatch);
             } else {
                 if (item.quality < 50) { //TODO: combine this condition and increaseQualityByOne
                     item.increaseQualityByOne();
@@ -19,7 +23,6 @@ class GildedRose {
                         if (item.sellIn < 11) {
                             item.increaseQualityByOne();
                         }
-
                         if (item.sellIn < 6) {
                             item.increaseQualityByOne();
                         }
@@ -27,12 +30,12 @@ class GildedRose {
                 }
             }
 
-            item.dropSellInByOneIf((Item item1) -> !item1.matchesName("Sulfuras, Hand of Ragnaros"));
+            item.dropSellInByOneIf(this.nonLegendaryItemMatch);
 
             if (item.sellIn < 0) {
                 if (!item.name.equals("Aged Brie")) {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.dropQualityByOneIf((Item item1) -> !item1.matchesName("Sulfuras, Hand of Ragnaros"));
+                        item.dropQualityByOneIf(this.nonLegendaryItemMatch);
                     } else {
                         item.quality = 0;
                     }

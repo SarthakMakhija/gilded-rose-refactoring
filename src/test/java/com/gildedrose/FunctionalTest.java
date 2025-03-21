908e62190158ef666dd4c09e6a0deb7dee0b98f6
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-03-21 18:44:51 +0530

    Sarthak | Refactors GildedRose, takes care of potential divergent change

--- src/test/java/com/gildedrose/FunctionalTest.java
+++ src/test/java/com/gildedrose/FunctionalTest.java
@@ -16,9 +16,7 @@ public class FunctionalTest {
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6)};
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)};
 
         GildedRose app = new GildedRose(items);
         app.updateQuality();
@@ -32,9 +30,7 @@ public class FunctionalTest {
                 new Item("Sulfuras, Hand of Ragnaros", -1, 80),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 13, 22),
                 new Item("Backstage passes to a TAFKAL80ETC concert", 8, 50),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 3, 50),
-                // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 1, 4)};
+                new Item("Backstage passes to a TAFKAL80ETC concert", 3, 50)};
 
         for (int index = 0; index < expected.length; index++) {
             assertEquals(expected[index].name, app.items[index].name);

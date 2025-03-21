1d13be5df675a80af01d34c5607a7f8f15e0975a
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-03-21 17:26:56 +0530

    Sarthak | Refactors updateQuality in GildedRose, introduces qualityUpdateActions

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,34 +1,36 @@
 package com.gildedrose;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 class GildedRose {
     Item[] items;
     Predicate<Item> nonLegendaryItemMatch;
+    Map<String, Consumer<Item>> qualityUpdateActions;
 
     public GildedRose(Item[] items) {
         this.items = items;
         this.nonLegendaryItemMatch = ((Predicate<Item>) (Item item) -> item.matchesName("Sulfuras, Hand of Ragnaros")).negate();
+        this.qualityUpdateActions = new HashMap<>();
+
+        this.qualityUpdateActions.put("Sulfuras, Hand of Ragnaros", (Item item) -> {});
+        this.qualityUpdateActions.put("Aged Brie", Item::increaseQualityByOne);
+        this.qualityUpdateActions.put("Backstage passes to a TAFKAL80ETC concert", (Item item) -> {
+            if (item.sellIn < 6)
+                item.increaseQualityBy(3);
+            else if (item.sellIn < 11)
+                item.increaseQualityBy(2);
+            else
+                item.increaseQualityByOne();
+        });
     }
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                item.dropQualityByOneIf(this.nonLegendaryItemMatch);
-            } else {
-                if (item.quality < 50) { //TODO: combine this condition and increaseQualityByOne
-                    item.increaseQualityByOne();
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            item.increaseQualityByOne();
-                        }
-                        if (item.sellIn < 6) {
-                            item.increaseQualityByOne();
-                        }
-                    }
-                }
-            }
+            Consumer<Item> action = this.qualityUpdateActions.getOrDefault(item.name, Item::dropQualityByOne);
+            action.accept(item);
 
             item.dropSellInByOneIf(this.nonLegendaryItemMatch);
 
